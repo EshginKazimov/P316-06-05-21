a5ac2522c0b6da804442875820cad17d1238cabb
EshginKazimov <82080797+EshginKazimov@users.noreply.github.com>
2021-05-07 09:24:44 +0400

    Add files via upload

---
+++ WebAppDatabaseConnection/WebAppDatabaseConnection/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppDatabaseConnection.DataAccessLayer;
+using WebAppDatabaseConnection.Models;
+
+namespace WebAppDatabaseConnection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //var students = new List<Student>
+            //{
+            //    new Student {Id = 1, Name = "Faig", Surname = "Abdullayev", Age = 21},
+            //    new Student {Id = 2, Name = "Nihat", Surname = "Osmanov", Age = 20},
+            //    new Student {Id = 3, Name = "Hashim", Surname = "Seyidov", Age = 20},
+            //    new Student {Id = 4, Name = "Nazim", Surname = "Hasanov", Age = 29},
+            //    new Student {Id = 5, Name = "Abdulaziz", Surname = "Karimli", Age = 20}
+            //};
+
+            //var students = _db.Students.Include(x => x.Group).Where(student => student.Group.Name == "P316").ToList();
+            var students = _db.Students.Include(x => x.Group).ToList();
+            var p316Students = students.Where(student => student.Group.Name == "P316").ToList();
+
+            return View(p316Students);
+        }
+    }
+}
---
+++ WebAppDatabaseConnection/WebAppDatabaseConnection/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebAppDatabaseConnection.DataAccessLayer;
+
+namespace WebAppDatabaseConnection
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var connectionSection = _configuration.GetSection("ConnectionStr");
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            //var connectionString = _configuration["ConnectionString:DefaultConnection"];
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
